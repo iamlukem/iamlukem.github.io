4d1a0193b617e062fcac33ebf84cbf3716dd0ed0
Luke Martin <luke@outofthesquare.com.au>
2016-07-24 23:06:00 +1000

    Removed baggage

--- library/javascripts/luke-martin.js
+++ library/javascripts/luke-martin.js
@@ -1,11 +1,11 @@
 
-		// Color Setup
-		// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+// Color Setup
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
-		var date = new Date();
-		var hours = date.getHours();
+var date = new Date();
+var hours = date.getHours();
 
-		// Adds a night time stylesheet
-		if ( hours <= 5 || hours >= 16 ){
-			document.documentElement.classList.add("night");
-		}
+// Adds a night time stylesheet
+if ( hours <= 5 || hours >= 16 ){
+	document.documentElement.classList.add("night");
+}
